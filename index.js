a3e06ff460ec8a08fe5927df1a96c7c569ac7245
L <L@L.com>
2025-01-08 13:56:16 -0800

    changes

---
+++ index.js
@@ -0,0 +1,23 @@
+<script>
+ function showUserinput(){
+let userInput= document.getElementByID("shoes"),value;
+ 
+
+</script>
+
+let Logan='index.html';
+
+function myFunction(){
+let x='hello';
+let y = document.getElementByID('shoes').value;
+let z=',hey there take a look around';
+let msg= x+y+z;
+const output= document.getElementbyID('msgOutput')
+output.style.display='block';
+output.innerHtml=msg;
+}
+
+
+
+function greeting(hello){
+ console.log ("welcome,",hello) 
